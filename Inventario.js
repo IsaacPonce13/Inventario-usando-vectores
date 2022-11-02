3f2ac292e01f312e7f094920ce5bff05e4b5379b
IsaacPonce13 <111556869+IsaacPonce13@users.noreply.github.com>
2022-11-01 21:32:30 -0600

    Add files via upload
    Inventario con listas dobles

--- Inventario.js
+++ Inventario.js
@@ -4,73 +4,128 @@ class Producto{
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.costo = costo;
+        this.sig = null;
+        this.ant = null;
     }
     Mostrar(){
         return `<p> Codigo: ${this.codigo} Nombre: ${this.nombre} Cantidad: ${this.cantidad} Costo: $${this.costo}</p>`;
     }
 }
-class Nodo{
-    constructor (NewProd){
-    this.Prod = NewProd;
-    this.next = null;
-    }
-}
+
 class Inventario{
     constructor(){
         this.primero = null;
+        this.ultimo = null;
+        this.size = 0;
     }
+
     agregar(NewProd){
-        if (this.primero == null)
+        let temp = null;
+        if (this.primero == null) {
+            this.ultimo = NewProd;
             this.primero = NewProd;
-        else{
-            let temp = this.primero;
-            while (temp.next != null)
-                temp = temp.next;
-                temp.next = NewProd;
+            this.primero.ant = null;
+            this.primero.sig = null;
+            this.size++;
+        }else if(NewProd.codigo < this.primero.codigo){
+            temp = this.primero;
+            NewProd.sig = temp;
+            temp.ant = NewProd;
+            this.primero = NewProd;
+            this.size++;
+        }else{
+            temp = this.primero;
+            while(temp.sig != null && temp.codigo < NewProd.codigo){
+                temp = temp.sig;
+            }
+            if(NewProd.codigo < temp.codigo){
+                NewProd.sig = temp;
+                NewProd.ant = temp.ant;
+                temp.ant.sig = NewProd;
+                temp.ant = NewProd;
+                this.size++;
+            }else if(NewProd.codigo > temp.codigo){
+                NewProd.ant = temp;
+                temp.sig = NewProd;
+                this.size++;
+            }
         }
+        temp = this.primero;
+            while(temp.sig != null){
+                temp = temp.sig;
+            }
+            this.ultimo = temp;
     }
 
     buscarProd(codigoProd){
         let temp = this.primero;
-        while (temp != null) {
-            if (temp.codigo = codigoProd) {
-                return temp
+        while(temp !== null) {
+            if (temp.codigo === codigoProd) {
+                return temp;
             }
-            temp = temp.next;
+            temp = temp.sig;
         }
         return null;
     }
+
     eliminarProd(codigoProd){
-        if (this.primero == null) {
-            return false;
-        }
         let temp = this.primero;
-        while (temp != null) {
-            if (temp.codigo = codigoProd) {
-                    temp = temp.next;
-                    temp.next = null;
+        while(temp.sig !== null) {
+            if (temp.codigo === codigoProd) {
+                if (temp.ant == null) {
+                    if (this.primero == null) {
+                        return null
+                    };
+                    if (this.primero === this.ultimo) {
+                        this.primero = null;
+                        this.ultimo = null;
+                    } else {
+                        this.primero = this.primero.sig;
+                        this.primero.ant = null;
+                    };
+                    this.size--;                
+                } else if (temp.sig == null) {
+                    if (this.ultimo == null) {
+                        return null
+                    };
+                    if (this.primero === this.ultimo) {
+                        this.primero = null;
+                        this.ultimo = null;
+                    } else {
+                        this.ultimo = this.ultimo.amt;
+                        this.ultimo.sig = null;
+                    };
+                    this.size--;
+                }else{
+                    temp.ant.sig = temp.sig;
+                    temp.sig.ant = temp.ant;
+                }
+                this.size--;
+                return temp;
             }
-            return temp;
+            temp.ant = temp;
+            temp = temp.sig;
         }
         return null;
-        
     }
+
     mostrarProd(){
-            let aux="";
-            let temp=this.primero;
-            while(temp!=null){
-                aux += temp + " ";
-                temp = temp.next;
-            }
-            return aux;
+        let temp = this.primero;
+        let res = "";
+        while(temp != null) {
+            res += temp.Mostrar();
+            temp = temp.sig;
+        }
+        return res;
     }
     mostrarProdInver(){
-        if (this.primero==null)
-        this.primero = NewProd;
-    else{
-        NewProd.next = this.primero;
-        this.primero = NewProd;       
+        let temp = this.ultimo;
+        let res = "";
+        while(temp != null) {
+            res += temp.Mostrar();
+            temp = temp.ant;
+        }
+        return res;
     }
-}
 
 }
